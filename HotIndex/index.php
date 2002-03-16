c1be90f62ece4b6051afe23a281dd07b9923ac12
Frenck <Frenck>
2002-03-16 13:02:05 +0000

    First file :D (nothing in it yet!)

---
+++ HotIndex/index.php
@@ -0,0 +1,36 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+/***************************************************************************/
+/* +----------------------------------------------------------------------+*/
+/* | HotIndex:                                                            |*/
+/* | Offers you a HotScript.com-a-like system for your own site!          |*/
+/* | Create your own script/downloads/link index!                         |*/
+/* +----------------------------------------------------------------------+*/
+/* |                                                                      |*/
+/* | Copyright (C) 2002  F.J.K. Nijhof, The Netherlands                   |*/
+/* |                                                                      |*/
+/* | This program is free software; you can redistribute it and/or        |*/
+/* | modify it under the terms of the GNU General Public License          |*/
+/* | as published by the Free Software Foundation; either version 2       |*/
+/* | of the License, or (at your option) any later version.               |*/
+/* |                                                                      |*/
+/* | This program is distributed in the hope that it will be useful,      |*/
+/* | but WITHOUT ANY WARRANTY; without even the implied warranty of       |*/
+/* | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |*/
+/* | GNU General Public License for more details.                         |*/
+/* |                                                                      |*/
+/* | You should have received a copy of the GNU General Public License    |*/
+/* | along with this program; if not, write to the Free Software          |*/
+/* | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA            |*/
+/* | 02111-1307, USA.                                                     |*/
+/* |                                                                      |*/
+/* | Author: F.J.K. Nijhof  Frenck@Frenck.nl   The Netherlands            |*/
+/* |                                                                      |*/
+/* +----------------------------------------------------------------------+*/
+/* | http://www.Frenck.nl                                                 |*/
+/* +----------------------------------------------------------------------+*/
+/***************************************************************************/
+
+
+
+?>
